59165495d4be3ef658f9dd1a1846e72c659a394e
JoLi0608 <wyli608@163.com>
2022-04-06 09:56:00 +0100

    multiple envseeds

--- loadmodel.py
+++ loadmodel.py
@@ -9,83 +9,11 @@ import ray.rllib.agents.ars as ars
 import ray.rllib.agents.sac as sac
 # from ray.rllib.agents.sac import SACTrainer
 # from starlette.requests import Request
-
-gym_env = "Hopper-v2"
-seed = 99
-check = 1
-algorithm = "SAC"
-trained_model = "/Users/liwenyu/Downloads/ray_results/SAC_Hopper-v2_1e4ba_00000_0_2022-03-30_13-27-17/checkpoint_000100/checkpoint-100"
-
-wandb.init(project="RTDM", entity="rt_dm")
-env = gym.make(gym_env)
-
-compute_times = []
-wconfig = wandb.config
-# wconfig.learning_timestep = 10000
-wconfig.algorithm = algorithm
-wconfig.seed = seed
-wconfig.env = gym_env
-wconfig.check = 1
-
-
-
-serve.start()
-trainer = sac.SACTrainer(
-    config={
-        "framework": "torch",
-        # "num_workers": 4,
-    },
-    env="Hopper-v2",
-)
-trainer.restore(trained_model)
-
-
-# myconfig = {
-#     # Environment (RLlib understands openAI gym registered strings).
-#     "env": "Hopper-v2",
-#     # Use 2 environment workers (aka "rollout workers") that parallelly
-#     # collect samples from their own environment clone(s).
-#     "num_workers": 2,
-#     # Change this to "framework: torch", if you are using PyTorch.
-#     # Also, use "framework: tf2" for tf2.x eager execution.
-#     "framework": "torch"
-
-# }
-
-# # trainer = ray.tune.run(PPOTrainer, config=myconfig, restore="/Users/liwenyu/Downloads/ray_results/PPO_Hopper-v2_0c9e7_00000_0_2022-03-30_13-26-47/checkpoint_004300/checkpoint-4300")
-
-# trainer = ray.tune.run(
-# "PPO",
-# #name="PPO_discrete5",
-# config=myconfig,
-# local_dir="/Users/liwenyu/Downloads/ray_results ",
-
-# checkpoint_freq=10, # iterations
-# checkpoint_at_end=True,
-# max_failures=100,
-# #resume=True,
-# restore=("/Users/liwenyu/Downloads/ray_results/PPO_Hopper-v2_0c9e7_00000_0_2022-03-30_13-26-47/checkpoint_004300/checkpoint-4300"),
-
-# #search_alg=algo,
-# #scheduler=ahb,
-# # 2 if testing, 50 or more for real
-# #num_samples=50,
-
-# stop={
-#     # "episode_reward_mean": 0,
-#     # "training_iteration": 1,
-#     # "timesteps_total": 1000,
-#     "episodes_total": 1000,
-# },)
-
 def play(env, trainer, times, asy = 0, level = 0):
     print('difficulty level:', level)
     total_rewards = []
     iter_ep = 20
-    total_ep = level*iter_ep
-    
-    
-    
+    total_ep = level*iter_ep    
 
     for k in range(iter_ep):
         # print("here")
@@ -129,14 +57,43 @@ def play(env, trainer, times, asy = 0, level = 0):
     wandb.log({"average_rewards": reward_ave, "difficulty_level": level})
     return reward_ave
 
-record = []
-reward_ave = play(env, trainer, 800, asy = 0)
-record.append(reward_ave)
-x = range(0,20)
-for level in x[1:]:
-    # print('here')
-    reward_ave = play(env, trainer, 800, asy = 1, level = level)
+env_seeds = [1,2,3,4,5]
+train_seed = 2
+
+for env_seed in env_seeds:
+    gym_env = "Hopper-v2"
+    check_point = 1
+    algorithm = "ARS"
+    trained_model = "/app/data/ray_results/2/ARS_CartPole-v0_661d3_00000_0_2022-03-31_10-07-40/checkpoint_000100"
+    wandb.init(project="RTDM", entity="rt_dm")
+    env = gym.make(gym_env)
+
+    compute_times = []
+    wconfig = wandb.config
+    # wconfig.learning_timestep = 10000
+    wconfig.algorithm = algorithm
+    wconfig.env_seed = env_seed
+    wconfig.train_seed = train_seed
+    wconfig.env = gym_env
+    wconfig.check = check_point
+
+    serve.start()
+    trainer = ars.ARSTrainer(
+        config={
+            "framework": "torch",
+            # "num_workers": 4,
+        },
+        env="Hopper-v2",
+    )
+    trainer.restore(trained_model)
+    record = []
+    reward_ave = play(env, trainer, 500, asy = 0)
     record.append(reward_ave)
-time_ave = sum(compute_times)/len(compute_times)
-wandb.log({'average_compute_time':time_ave})
-#print('final result:' , record)
+    x = range(0,20)
+    for level in x[1:]:
+        # print('here')
+        reward_ave = play(env, trainer, 500, asy = 1, level = level)
+        record.append(reward_ave)
+    time_ave = sum(compute_times)/len(compute_times)
+    wandb.log({'average_compute_time':time_ave})
+    #print('final result:' , record)
