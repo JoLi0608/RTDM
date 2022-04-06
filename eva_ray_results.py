d22ef7fb54486e3e397523f939ca38af88297640
JoLi0608 <wyli608@163.com>
2022-04-06 11:01:43 +0100

    update

--- eva_ray_results.py
+++ eva_ray_results.py
@@ -57,43 +57,42 @@ def play(env, trainer, times, asy = 0, level = 0):
     wandb.log({"average_rewards": reward_ave, "difficulty_level": level})
     return reward_ave
 
-env_seeds = [1,2,3,4,5]
+env_seeds = 1
 train_seed = 2
 
-for env_seed in env_seeds:
-    gym_env = "Hopper-v2"
-    check_point = 1
-    algorithm = "ARS"
-    trained_model = "/app/data/ray_results/2/ARS_CartPole-v0_661d3_00000_0_2022-03-31_10-07-40/checkpoint_000100"
-    wandb.init(project="RTDM", entity="rt_dm")
-    env = gym.make(gym_env)
+# for env_seed in env_seeds:
+gym_env = "Hopper-v2"
+check_point = 1
+algorithm = "ARS"
+trained_model = "/Users/liwenyu/Downloads/ray_results/ARS_Hopper-v2_303ed_00000_0_2022-03-30_13-27-47/checkpoint_000100/checkpoint-100"
+wandb.init(project="RTDM", entity="rt_dm")
+env = gym.make(gym_env)
 
-    compute_times = []
-    wconfig = wandb.config
-    # wconfig.learning_timestep = 10000
-    wconfig.algorithm = algorithm
-    wconfig.env_seed = env_seed
-    wconfig.train_seed = train_seed
-    wconfig.env = gym_env
-    wconfig.check = check_point
-
-    serve.start()
-    trainer = ars.ARSTrainer(
-        config={
-            "framework": "torch",
-            # "num_workers": 4,
-        },
-        env="Hopper-v2",
-    )
-    trainer.restore(trained_model)
-    record = []
-    reward_ave = play(env, trainer, 500, asy = 0)
+compute_times = []
+wconfig = wandb.config
+# wconfig.learning_timestep = 10000
+wconfig.algorithm = algorithm
+wconfig.env_seed = env_seeds
+wconfig.train_seed = train_seed
+wconfig.env = gym_env
+wconfig.check = check_point
+serve.start()
+trainer = ars.ARSTrainer(
+    config={
+        "framework": "torch",
+        # "num_workers": 4,
+    },
+    env=gym_env,
+)
+trainer.restore(trained_model)
+record = []
+reward_ave = play(env, trainer, 500, asy = 0)
+record.append(reward_ave)
+x = range(0,20)
+for level in x[1:]:
+    # print('here')
+    reward_ave = play(env, trainer, 500, asy = 1, level = level)
     record.append(reward_ave)
-    x = range(0,20)
-    for level in x[1:]:
-        # print('here')
-        reward_ave = play(env, trainer, 500, asy = 1, level = level)
-        record.append(reward_ave)
-    time_ave = sum(compute_times)/len(compute_times)
-    wandb.log({'average_compute_time':time_ave})
-    #print('final result:' , record)
+time_ave = sum(compute_times)/len(compute_times)
+wandb.log({'average_compute_time':time_ave})
+#print('final result:' , record)
