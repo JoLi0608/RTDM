4eb36749962ee053e162b78177db3d4179c7566d
JoLi0608 <wyli608@163.com>
2022-04-15 12:11:03 +0100

    test

--- evaluation.py
+++ evaluation.py
@@ -128,19 +128,19 @@ if type == 'mbrl':
         env = humanoid.HumanoidTruncatedObsEnv()
     elif environment == 'cartpole_continuous':
         env = cart.CartPoleEnv()
-    # trainer = load_agent(args["modelpath"],env)
+    trainer = load_agent(args["modelpath"],env)
     # path = "/app/data/pets/HalfCheetah-v2/102236/"
 
-    cfg = omegaconf.OmegaConf.load(args["modelpath"]+".hydra/config.yaml")
-    #cfg["device"] = "cpu"
-    torch_generator = torch.Generator(device=cfg.device)
-    env, term_fn, reward_fn = mbrl.util.env.EnvHandler.make_env(cfg)
-    obs_shape = env.observation_space.shape
-    act_shape = env.action_space.shape
-    dynamics_model = mbrl.util.common.create_one_dim_tr_model(cfg, obs_shape, act_shape)
-    dynamics_model.load(args["modelpath"])
-    model_env = mbrl.models.ModelEnv(env, dynamics_model, term_fn, reward_fn)
-    trainer = mbrl.planning.create_trajectory_optim_agent_for_model(model_env, cfg.algorithm.agent, num_particles=cfg.algorithm.num_particles)
+    # cfg = omegaconf.OmegaConf.load(args["modelpath"]+".hydra/config.yaml")
+    # #cfg["device"] = "cpu"
+    # torch_generator = torch.Generator(device=cfg.device)
+    # env, term_fn, reward_fn = mbrl.util.env.EnvHandler.make_env(cfg)
+    # obs_shape = env.observation_space.shape
+    # act_shape = env.action_space.shape
+    # dynamics_model = mbrl.util.common.create_one_dim_tr_model(cfg, obs_shape, act_shape)
+    # dynamics_model.load(args["modelpath"])
+    # model_env = mbrl.models.ModelEnv(env, dynamics_model, term_fn, reward_fn)
+    # trainer = mbrl.planning.create_trajectory_optim_agent_for_model(model_env, cfg.algorithm.agent, num_particles=cfg.algorithm.num_particles)
 
 
 elif type == 'rtrl':
