7d79c8d100b7cd0c2f62f146a7a20aa31a241e07
JoLi0608 <wyli608@163.com>
2022-04-19 16:02:31 +0100

    add pets

--- evaluation.py
+++ evaluation.py
@@ -103,11 +103,12 @@ type = args["modeltype"]
 seed = int(args["evaseed"])
 environment = args["envir"]
 path = args["modelpath"]
+algorithm = args["algorithm"]
 
 wandb.init(project="RTDM", entity="rt_dm")
 wconfig = wandb.config
 wconfig.model_type = type
-wconfig.algorithm = args["algorithm"]
+wconfig.algorithm = algorithm
 wconfig.eva_seed = args["evaseed"]
 wconfig.train_seed = args["trainseed"]
 wconfig.env = environment
@@ -131,20 +132,22 @@ if type == 'mbrl':
         env = cart.CartPoleEnv()
     else:
         env = gym.make(environment)
-    # trainer = load_agent(path,env)
-    # path = "/app/data/pets/HalfCheetah-v2/102236/"
+    if algorithm == 'mbpo':
+        trainer = load_agent(path,env)
+    elif algorithm == 'pets':
+        cfg = omegaconf.OmegaConf.load(path+"/.hydra/config.yaml")
+        #cfg["device"] = "cpu"
+        torch_generator = torch.Generator(device=cfg.device)
+        env, term_fn, reward_fn = mbrl.util.env.EnvHandler.make_env(cfg)
+        obs_shape = env.observation_space.shape
+        act_shape = env.action_space.shape
+        dynamics_model = mbrl.util.common.create_one_dim_tr_model(cfg, obs_shape, act_shape)
+        dynamics_model.load(path)
+        model_env = mbrl.models.ModelEnv(env, dynamics_model, term_fn, reward_fn)
+        agent = mbrl.planning.create_trajectory_optim_agent_for_model(model_env, cfg.algorithm.agent, num_particles=cfg.algorithm.num_particles)
+        action = agent.act(env.reset())
+        action = np.clip(action, -1.0, 1.0)  # to account for the noise
 
-    cfg = omegaconf.OmegaConf.load(path+".hydra/config.yaml")
-    #cfg["device"] = "cpu"
-    torch_generator = torch.Generator(device=cfg.device)
-
-    env, term_fn, reward_fn = mbrl.util.env.EnvHandler.make_env(cfg)
-    obs_shape = env.observation_space.shape
-    act_shape = env.action_space.shape
-    dynamics_model = mbrl.util.common.create_one_dim_tr_model(cfg, obs_shape, act_shape)
-    dynamics_model.load(path)
-    model_env = mbrl.models.ModelEnv(env, dynamics_model, term_fn, reward_fn)
-    trainer = mbrl.planning.create_trajectory_optim_agent_for_model(model_env, cfg.algorithm.agent, num_particles=cfg.algorithm.num_particles)
 
 
 elif type == 'rtrl':
