6c56d37dbc2c26522c858d18448d568322dcb735
JoLi0608 <wyli608@163.com>
2022-04-20 17:17:30 +0100

    minor

--- evaluation.py
+++ evaluation.py
@@ -78,7 +78,7 @@ def play(env, trainer, times, flag, gap, type, algorithm, level = 0):
             wandb.log({"computation_time": compute_time})
             compute_times.append(compute_time)
             obs, reward, done, info = env.step(action)
-            # print(level, done, i)
+            print(level, done, i)
             repeat = int(level * 1 * compute_time)
             total_reward += reward
             if repeat:
