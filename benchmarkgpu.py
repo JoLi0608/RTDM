973e1147f4d18ab2479d4e43f21cc5f03be73a66
pierthodo <pierthodo@gmail.com>
2022-04-23 11:24:30 +0100

    Update benchmarkgpu.py

--- benchmarkgpu.py
+++ benchmarkgpu.py
@@ -12,6 +12,59 @@ import mbrl.planning
 import numpy as np
 import rtrl
 
+def load(algo,gpu=False):
+    if algo == "mbpo":
+        env = gym.make("Hopper-v2")
+        device = "cuda" if gpu else "cpu"
+        ag = load_agent("/app/data/mbpo/default/Hopper-v2/2022.04.01/100721", env,device)
+        agent = lambda obs: ag.act(obs)
+    elif algo == "pets":
+        path = "/app/data/pets/Hopper-v2/091637/"
+        cfg = omegaconf.OmegaConf.load(path+".hydra/config.yaml")
+        if not gpu:
+            cfg["device"] = "cpu"
+        torch_generator = torch.Generator(device=cfg.device)
+        env, term_fn, reward_fn = mbrl.util.env.EnvHandler.make_env(cfg)
+        dynamics_model = mbrl.util.common.create_one_dim_tr_model(cfg, env.observation_space.shape, env.action_space.shape)
+        dynamics_model.load(path)
+        if gpu == False:
+            dynamics_model.to("cpu")
+        model_env = mbrl.models.ModelEnv(env, dynamics_model, term_fn, reward_fn)
+        ag = mbrl.planning.create_trajectory_optim_agent_for_model(model_env, cfg.algorithm.agent, num_particles=cfg.algorithm.num_particles)
+        action = ag.act(env.reset())
+        action = np.clip(action, -1.0, 1.0)  # to account for the noise
+        agent = lambda obs: np.clip(ag.act(obs),-1.0,1.0)
+   elif algo == "ars"
+        path = "/app/data/ray_results/1/ARS_Hopper-v2_47175_00000_0_2022-04-07_11-24-24/checkpoint_015300/checkpoint-15300"
+        num_gpus = 0.2 if gpu else 0
+        trainer = ars.ARSTrainer(
+            config={
+                "framework": "torch",
+                "num_gpus": num_gpus,
+            },
+            env="Hopper-v2",
+        )
+        trainer.restore(path)
+        agent = lambda obs: trainer.compute_single_action(obs)
+        env = gym.make("Hopper-v2")
+    elif algo == "rtrl":
+        path = "/app/data/rtrl_2/exp-1-Hopper-v2-RTAC/"
+        r = rtrl.load(path+"state")
+        if not gpu:
+            r.agent.model.to("cpu")
+        else:
+            r.agent.model.to("cuda")
+        agent = lambda obs: r.agent.act(obs,[],[],[],train=False)[0]
+        env = gym.make("Hopper-v2")
+
+    elif algo == "sac":
+        device = "cuda" if gpu else "cpu"
+        env,agent = load_policy_and_env("/app/data/Hopper-v2/cmd_sac_pytorch/cmd_sac_pytorch_s1",device=device)
+    elif algo == "PPO":
+        device = "cuda" if gpu else "cpu"
+        env,agent = load_policy_and_env("/app/data/Hopper-v2/cmd_ppo_pytorch/cmd_ppo_pytorch_s1",device=device)
+    return agent,env
+    
 def run_env(agent,env,num_steps=1000,conc_prev=False):
     prev_action = np.zeros(env.action_space.shape[0])
     t1 = time.time()
@@ -24,129 +77,15 @@ def run_env(agent,env,num_steps=1000,conc_prev=False):
         prev_action = action
     return (time.time()-t1)/1000
 
-#def load_agent(algo="PPO",path=,device="cpu"):
-bench_gpu = False
 
-
-print("Benchmark mbpo")
-env = gym.make("Hopper-v2")
-ag = load_agent("/app/data/mbpo/default/Hopper-v2/2022.04.01/100721", env,"cpu")
-
-agent = lambda obs: ag.act(obs)
-
-t = run_env(agent,env)
-print("Average CPU inference time: ",t)
-
-
-if bench_gpu:
-    ag = load_agent("/app/data/mbpo/default/Hopper-v2/2022.04.01/100721", env,"cuda")
-
-    agent = lambda obs: ag.act(obs)
-
-    t = run_env(agent,env)
-    print("Average GPU inference time: ",t)
-
-print("Benchmark pets")
-
-path = "/app/data/pets/Hopper-v2/091637/"
-if bench_gpu:
-
-    cfg = omegaconf.OmegaConf.load(path+".hydra/config.yaml")
-    #cfg["device"] = "cpu"
-    torch_generator = torch.Generator(device=cfg.device)
-
-    env, term_fn, reward_fn = mbrl.util.env.EnvHandler.make_env(cfg)
-    obs_shape = env.observation_space.shape
-    act_shape = env.action_space.shape
-    dynamics_model = mbrl.util.common.create_one_dim_tr_model(cfg, obs_shape, act_shape)
-    dynamics_model.load(path)
-    model_env = mbrl.models.ModelEnv(env, dynamics_model, term_fn, reward_fn)
-    ag = mbrl.planning.create_trajectory_optim_agent_for_model(model_env, cfg.algorithm.agent, num_particles=cfg.algorithm.num_particles)
-    action = ag.act(env.reset())
-    action = np.clip(action, -1.0, 1.0)  # to account for the noise
-    agent = lambda obs: np.clip(ag.act(obs),-1.0,1.0)
-
-    t = run_env(agent,env,num_steps=20)
-    print("Average GPU inference time: ",t)
-
-
-
-cfg = omegaconf.OmegaConf.load(path+".hydra/config.yaml")
-cfg["device"] = "cpu"
-torch_generator = torch.Generator(device=cfg.device)
-
-env, term_fn, reward_fn = mbrl.util.env.EnvHandler.make_env(cfg)
-obs_shape = env.observation_space.shape
-act_shape = env.action_space.shape
-dynamics_model = mbrl.util.common.create_one_dim_tr_model(cfg, obs_shape, act_shape)
-dynamics_model.load(path)
-dynamics_model.to("cpu")
-model_env = mbrl.models.ModelEnv(env, dynamics_model, term_fn, reward_fn)
-ag = mbrl.planning.create_trajectory_optim_agent_for_model(model_env, cfg.algorithm.agent, num_particles=cfg.algorithm.num_particles)
-action = ag.act(env.reset())
-action = np.clip(action, -1.0, 1.0)  # to account for the noise
-agent = lambda obs: np.clip(ag.act(obs),-1.0,1.0)
-
-t = run_env(agent,env,num_steps=20)
-print("Average CPU inference time: ",t)
-
-
-
-print("Benchmark rllib")
-path = "/app/data/ray_results/1/ARS_Hopper-v2_47175_00000_0_2022-04-07_11-24-24/checkpoint_015300/checkpoint-15300"
-trainer = ars.ARSTrainer(
-    config={
-        "framework": "torch",
-    },
-    env="Hopper-v2",
-)
-trainer.restore(path)
-agent = lambda obs: trainer.compute_single_action(obs)
-env = gym.make("HalfCheetah-v2")
-t = run_env(agent,env)
-print("Average CPU inference time: ",t)
-
-if bench_gpu:
-
-    trainer = ars.ARSTrainer(
-        config={
-            "framework": "torch",
-            "num_gpus":0.2,
-        },
-        env="HalfCheetah-v2",
-    )
-    trainer.restore(path)
-    agent = lambda obs: trainer.compute_single_action(obs)
-    env = gym.make("HalfCheetah-v2")
-    t = run_env(agent,env)
-    print("Average GPU inference time: ",t)
-
-
-print("Benchmark RTRL --------")
-path = "/app/data/rtrl_2/exp-1-Hopper-v2-RTAC/"
-r = rtrl.load(path+"state")
-r.agent.model.to("cpu")
-agent = lambda obs: r.agent.act(obs,[],[],[],train=False)[0]
-env = gym.make("Hopper-v2")
-
-t = run_env(agent,env,conc_prev=True)
-print("Average CPU inference time: ",t)
-if bench_gpu:
-    r.agent.model.to("cuda")
-    t = run_env(agent,env,conc_prev=True)
-    print("Average GPU inference time: ",t)
-
-
-# Start with spinup
-print("Benchmark spinup -------")
-env,policy = load_policy_and_env("/app/data/Hopper-v2/cmd_sac_pytorch/cmd_sac_pytorch_s1",device="cpu")
-t = run_env(policy,env)
-print("Average CPU inference time: ",t)
-
-if bench_gpu:
-    env, policy = load_policy_and_env("/app/data/Hopper-v2/cmd_sac_pytorch/cmd_sac_pytorch_s1", device="gpu")
-    t = run_env(policy,env)
-    print("Average GPU inference time: ",t)
-
-
+inf_time = {}
+for gpu in [False,True]:
+    for algo in ["mbpo","pets","ars","rtrl","sac","ppo"]:
+        agent,env = load(algo,gpu=gpu)
+        if algo == "rtrl":
+            inf_time[algo+"_"+str(gpu)] = run_env(agent,env,conc_prev=True)
+        else:   
+            inf_time[algo+"_"+str(gpu)] = run_env(agent,env,conc_prev=True)
+            
+print(inf_time)
 
