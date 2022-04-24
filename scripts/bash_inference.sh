7a3d81d6577cf6a408a303dcd93362a88a5b07bd
pierthodo <pierthodo@gmail.com>
2022-04-24 10:47:26 +0100

    Update bash_inference.sh

--- scripts/bash_inference.sh
+++ scripts/bash_inference.sh
@@ -5,7 +5,7 @@
 for d in /app/data/spinup/sac/* ;do
     for n in $d/cmd_sac_pytorch/*/; do
         echo $n
-        python /app/RTDM/scripts/benchmarkgpu.py --path $n --algo sac --cpu $1
+        python /app/RTDM/scripts/benchmarkgpu.py --path $n --algo sac --cpu $1 --gpu $2
         break
     done
 done
@@ -15,7 +15,7 @@ for d in /app/data/mbpo/default/*/ ;do
     for n in $d*/; do
         for s in $n*/; do
             echo $s
-            python /app/RTDM/scripts/benchmarkgpu.py --path $s --algo mbpo --cpu $1
+            python /app/RTDM/scripts/benchmarkgpu.py --path $s --algo mbpo --cpu $1 --gpu $2
             break
         done
     done
@@ -28,7 +28,7 @@ do
     for n in $d/cmd_ppo_pytorch/*/
     do
         echo $n
-        python /app/RTDM/scripts/benchmarkgpu.py --path $n --algo ppo --cpu $1
+        python /app/RTDM/scripts/benchmarkgpu.py --path $n --algo ppo --cpu $1 --gpu $2
         break
     done
 done
@@ -40,7 +40,7 @@ do
     for env in continuous_CartPole-v0 Hopper-v2 Humanoid-v2 HalfCheetah-v2 Pusher-v2
     do
         for n in $d/ARS_$env*/ ; do
-          python /app/RTDM/scripts/benchmarkgpu.py --path $n --algo ars --cpu $1
+          python /app/RTDM/scripts/benchmarkgpu.py --path $n --algo ars --cpu $1 --gpu $2
           echo $n
         done
     done
@@ -53,7 +53,7 @@ done
 
 for d in /app/data/rtrl_2/*/ ;do
     echo $d
-    python /app/RTDM/scripts/benchmarkgpu.py --path $d --algo rtrl --cpu $1
+    python /app/RTDM/scripts/benchmarkgpu.py --path $d --algo rtrl --cpu $1 --gpu $2
 done
 
 
@@ -61,7 +61,7 @@ done
 for d in /app/data/pets/*/ ;do
     for n in $d*/; do
         echo $n
-        python /app/RTDM/scripts/benchmarkgpu.py --path $n --algo pets --cpu $1 
+        python /app/RTDM/scripts/benchmarkgpu.py --path $n --algo pets --cpu $1 --gpu $2
         break
     done
 done
