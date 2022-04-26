1b2501a7eaa3ab5600060b1cc78af8199aae3a76
pierthodo <pierthodo@gmail.com>
2022-04-26 21:33:25 +0100

    Update bash_repeat.sh

--- scripts/bash_repeat.sh
+++ scripts/bash_repeat.sh
@@ -14,74 +14,74 @@
 
 
 
-
-
-## SAC
-for d in /app/data/spinup/sac/* ;do
-    for n in $d/cmd_sac_pytorch/*/; do
-        echo $n
-        python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo sac --evaseed 1 &
-    done
-    wait
-done
-
-# MBPO
-for d in /app/data/mbpo/default/*/ ;do
-    for n in $d*/; do
-        for s in $n*/; do
-            echo $s
-            python /app/RTDM/scripts/evaluate_repeat.py --path $s --algo mbpo --evaseed 1 --gpu 1 &
+for seed in 1 2 3 4 5
+do
+    ## SAC
+    for d in /app/data/spinup/sac/* ;do
+        for n in $d/cmd_sac_pytorch/*/; do
+            echo $n
+            python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo sac --evaseed $seed &
         done
         wait
     done
-done
 
-
-# PETS
-#for d in /app/data/pets/*/ ;do
-#    for n in $d*/; do
-#        echo $n
-#        python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo pets --evaseed 1 --gpu 1 &
-#    done
-#    wait
-#done
-
-
-
-# ARS
-
-for d in /app/data/ray_results/*
-do
-    for env in continuous_CartPole-v0 Hopper-v2 Humanoid-v2 HalfCheetah-v2 Pusher-v2
-    do
-        for n in $d/ARS_$env*/ ; do
-          echo $n
-          python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo ars --evaseed 1 &
+    # MBPO
+    for d in /app/data/mbpo/default/*/ ;do
+        for n in $d*/; do
+            for s in $n*/; do
+                echo $s
+                python /app/RTDM/scripts/evaluate_repeat.py --path $s --algo mbpo --evaseed $seed --gpu 1 &
+            done
+            wait
         done
     done
-    wait
-done
 
-# PPO
-for d in /app/data/spinup/ppo/*
-do
-    for n in $d/cmd_ppo_pytorch/*/
+
+    # PETS
+    #for d in /app/data/pets/*/ ;do
+    #    for n in $d*/; do
+    #        echo $n
+    #        python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo pets --evaseed 1 --gpu 1 &
+    #    done
+    #    wait
+    #done
+
+
+
+    # ARS
+
+    for d in /app/data/ray_results/*
     do
-        echo $n
-        python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo ppo --evaseed 1 &
+        for env in continuous_CartPole-v0 Hopper-v2 Humanoid-v2 HalfCheetah-v2 Pusher-v2
+        do
+            for n in $d/ARS_$env*/ ; do
+              echo $n
+              python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo ars --evaseed $seed &
+            done
+        done
+        wait
     done
-    wait
+
+    # PPO
+    for d in /app/data/spinup/ppo/*
+    do
+        for n in $d/cmd_ppo_pytorch/*/
+        do
+            echo $n
+            python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo ppo --evaseed $seed &
+        done
+        wait
+    done
+
+
+    # RTRL
+
+
+    for d in /app/data/rtrl_3/exp/*/ ;do
+        echo $d
+        python /app/RTDM/scripts/evaluate_repeat.py --path $d --algo rtrl --evaseed $seed
+    done
 done
-
-
-# RTRL
-
-
-for d in /app/data/rtrl_3/exp/1-*/ ;do
-    echo $d
-    python /app/RTDM/scripts/evaluate_repeat.py --path $d --algo rtrl --evaseed 1
-done
-
 
 
 
