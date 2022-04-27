2be30ecba64cd7b34d6e478f83a699fce61ec884
pierthodo <pierthodo@gmail.com>
2022-04-27 10:24:50 +0100

    Update upload_inference.py

--- scripts/upload_inference.py
+++ scripts/upload_inference.py
@@ -4,14 +4,13 @@ import numpy as np
 
 
 d = pickle.load(open("/app/data/inference_time/data.pkl","rb"))
-for k in d.keys():
-    cpu = d.split("_")[1]
-    algorithm = d.split("_")[2]
-    env_name = d.split("_")[3]
-    if env_name == "continuous":
-        env_name =  d.split("_")[3]+"_"+ d.split("_")[4]
-        gpu =  d.split("_")[5]
-    else:
-        gpu = 4
-    time = np.median(d[k])
-wandb.init(project="RTDM_inference", entity="pierthodo")
+
+for algo in ["ars","mbpo","sac","ppo","rtrl"]:
+    for env in ["Hopper-v2","HalfCheetah-v2","continuous_CartPole-v0","Humanoid-v2","Pusher-v2"]:
+        run = wandb.init(project="RTDM_inference", entity="pierthodo")
+        for cpu in [4,1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.09,0.08,0.07,0.06,0.05,0.04,0.03,0.02]:
+            wconfig = wandb.config
+            wconfig.algorithm = algo
+            wconfig.env = env
+            wandb.log({"inference time":np.median(d["cpu_"+cpu+"_"+algo+"_"+env+"_gpu_0"],"cpu":cpu})
+        run.finish()
