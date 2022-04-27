5b1dda4fd7fd417cf922aa556a3743c1b094da88
pierthodo <pierthodo@gmail.com>
2022-04-27 14:50:10 +0100

    Update upload_inference.py

--- scripts/upload_inference.py
+++ scripts/upload_inference.py
@@ -5,12 +5,17 @@ import numpy as np
 
 d = pickle.load(open("/app/data/inference_time/data.pkl","rb"))
 
-for algo in ["ars","mbpo","sac","ppo","rtrl"]:
-    for env in ["Hopper-v2","HalfCheetah-v2","continuous_CartPole-v0","Humanoid-v2","Pusher-v2"]:
-        run = wandb.init(project="RTDM_inference", entity="pierthodo")
-        for cpu in [4,1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.09,0.08,0.07,0.06,0.05,0.04,0.03,0.02]:
-            wconfig = wandb.config
-            wconfig.algorithm = algo
-            wconfig.env = env
-            wandb.log({"inference time":np.median(d["cpu_"+str(cpu)+"_"+algo+"_"+env+"_gpu_0"]),"cpu":cpu})
-        run.finish()
+   
+for k in d.keys()
+    run = wandb.init(project="RTDM_inference", entity="pierthodo")
+    wconfig = wandb.config
+    wconfig.algorithm = k.split("_")[2]
+    wconfig.cpu = k.split("_")[1]
+    if "continuous" in k:
+        wconfig.env = "continuous-CartPole-v0"
+        wconfig.gpu = k.split("_")[6]
+    else
+        wconfig.env = k.split("_")[3]
+        wconfig.gpu = k.split("_")[5]
+    wandb.log({"inference time":np.median(d["cpu_"+str( wconfig.cpu )+"_"+wconfig.algorithm +"_"+wconfig.env+"_gpu_"+k.split("_")[6]]),"cpu":cpu})
+    run.finish()
