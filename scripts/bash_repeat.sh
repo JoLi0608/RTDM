620dc80c40562e9b2f77591ce3fb1206a8652f4d
pierthodo <pierthodo@gmail.com>
2022-04-27 20:07:22 +0100

    Update bash_repeat.sh

--- scripts/bash_repeat.sh
+++ scripts/bash_repeat.sh
@@ -2,39 +2,42 @@
 
 
 # Plot planet
-#for d in /app/data/planet/default/* ;do
-#    for n in $d/*; do
-#        for s in $n/*/; do
-#            echo $s
-#            python /app/RTDM/scripts/benchmarkgpu.py --path $s --algo planet --cpu $1 --gpu $2
-#            break
-#        done
-#    done
-#done
+
 
 
 
 for seed in 1 2 3 4 5
 do
-    # SAC
-    for d in /app/data/spinup/sac/* ;do
-        for n in $d/cmd_sac_pytorch/*/; do
-            echo $n
-            python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo sac --evaseed $seed --gpu 1&
-        done
-        wait
-    done
 
-    # MBPO
-    for d in /app/data/mbpo/default/*/ ;do
-        for n in $d*/; do
-            for s in $n*/; do
+    for d in /app/data/planet/default/* ;do#
+        for n in $d/*; do
+            for s in $n/*/; do
                 echo $s
-                python /app/RTDM/scripts/evaluate_repeat.py --path $s --algo mbpo --evaseed $seed --gpu 1 &
+                python /app/RTDM/scripts/evaluate_repeat.py --path $s --algo planet --evaseed $seed --gpu 1 &
+                break
             done
             wait
         done
     done
+    # SAC
+    #for d in /app/data/spinup/sac/* ;do
+    #    for n in $d/cmd_sac_pytorch/*/; do
+    #        echo $n
+    #        python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo sac --evaseed $seed --gpu 1&
+    #    done
+    #    wait
+    #done
+
+    # MBPO
+    #for d in /app/data/mbpo/default/*/ ;do
+    #    for n in $d*/; do
+    #        for s in $n*/; do
+    #            echo $s
+    #            python /app/RTDM/scripts/evaluate_repeat.py --path $s --algo mbpo --evaseed $seed --gpu 1 &
+    #        done
+    #        wait
+    #    done
+    #done
 
 
     # PETS
@@ -50,37 +53,37 @@ do
 
     # ARS
 
-    for d in /app/data/ray_results/*
-    do
-        for env in continuous_CartPole-v0 Hopper-v2 Humanoid-v2 HalfCheetah-v2 Pusher-v2
-        do
-            for n in $d/ARS_$env*/ ; do
-              echo $n
-              python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo ars --evaseed $seed --gpu 1 &
-            done
-        done
-        wait
-    done
+    #for d in /app/data/ray_results/*
+    #do
+    #    for env in continuous_CartPole-v0 Hopper-v2 Humanoid-v2 HalfCheetah-v2 Pusher-v2
+    #    do
+    #        for n in $d/ARS_$env*/ ; do
+    #          echo $n
+    #          python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo ars --evaseed $seed --gpu 1 &
+    #        done
+    #    done
+    #    wait
+    #done
 
     # PPO
-    for d in /app/data/spinup/ppo/*
-    do
-        for n in $d/cmd_ppo_pytorch/*/
-        do
-            echo $n
-            python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo ppo --evaseed $seed --gpu 1 &
-        done
-        wait
-    done
+    #for d in /app/data/spinup/ppo/*
+    #do
+    #    for n in $d/cmd_ppo_pytorch/*/
+    #    do
+    #        echo $n
+    #        python /app/RTDM/scripts/evaluate_repeat.py --path $n --algo ppo --evaseed $seed --gpu 1 &
+    #    done
+    #    wait
+    #done
 
 
     # RTRL
 
 
-    for d in /app/data/rtrl_3/exp/*/ ;do
-        echo $d
-        python /app/RTDM/scripts/evaluate_repeat.py --path $d --algo rtrl --evaseed $seed --gpu 1
-    done
+    #for d in /app/data/rtrl_3/exp/*/ ;do
+    #    echo $d
+    #    python /app/RTDM/scripts/evaluate_repeat.py --path $d --algo rtrl --evaseed $seed --gpu 1
+    #done
 done
 
 
