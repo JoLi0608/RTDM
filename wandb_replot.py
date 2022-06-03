d145a0e41f8a236ef1c39ad4729750d84f6869d5
JoLi0608 <wyli608@163.com>
2022-06-03 01:34:46 +0100

    clean up

--- wandb_replot.py
+++ wandb_replot.py
@@ -1,3 +1,14 @@
+# import wandb
+# api = wandb.Api()
+# run = api.run("pierthodo/RTDM_performance/mehq2vmg")
+# print(run.history())
+# print(run.config)
+
+import pandas as pd 
+import wandb
+import matplotlib.pyplot as plt
+import math
+
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
@@ -312,161 +323,26 @@ inf_dict = {'cpu_5_rtrl_HalfCheetah-v2_gpu_0': [0.00095501, 0.00095452, 0.000971
        1.65980492, 1.66768084, 1.67175648, 1.67757442, 1.66108391], 'cpu_4_planet_dmc_cheetah_run_gpu_0': [1.68951499, 1.7034822 , 1.72020984, 1.70672035, 1.69941554,
        1.73018804, 1.72154732, 1.7118628 , 1.7172157 , 1.69086418], 'cpu_4_planet_dmc_walker_walk_gpu_0': [1.70952363, 1.73020468, 1.70921488, 1.73887095, 1.74161286,
        1.71957116, 1.70842655, 1.71277156, 1.72997591, 1.78979383]}
-# 1. same algorithm same condition, difference of different environment
-# envs = ['Hopper-v2','Pusher-v2','continuous','HalfCheetah-v2','Humanoid-v2']
-# # envs = ['Hopper-v2','Pusher-v2','continuous','HalfCheetah-v2']
-# algs = ['ars','sac','ppo','rtrl','mbpo','pets']
-# # algs = ['sac','ppo','mbpo','rtrl']
-# # algs = ['ars','sac','ppo','mbpo','rtrl']
-# cpus = ['5','4','2','1','0.5','0.1','0.05','0.01']
-# gpu = ['1','0']
-
-# def testenv(cpu, gpu, alg, envs, dic = inf_dict):
-#     res = {}
-#     for exp, data in dic.items():
-#         # print(exp,data)
-#         ave_inf = sum(data)/len(data)
-#         exp = exp.split("_")
-#         # print((exp[1] == cpu), (exp[2] == alg and exp[-1] == gpu))
-#         if exp[1] == cpu and exp[2] == alg and exp[-1] == gpu and exp[3] in envs:
-#             env = exp[3]
-#             res[env] = ave_inf
-#     if alg == 'pets':
-#         res['Humanoid-v2'] = 0
-#     return res 
-# envlabel = ['Hopper','Pusher','CartPole','HalfCheetah','Humanoid']
-# result = []
-# bar_width = 0.15
-# objects = envlabel
-# y_pos = np.arange(len(objects))
-# for alg in algs:
-#     # for cpu in cpus:
-#     dicenv = testenv('5', '0', alg, envs)
-#     result.append(dicenv)
-# n = len(result)
-# for i in range(n):
-#     inference = []
-#     dicenv = result[i]
-#     print(i)
-#     print(dicenv,type(dicenv))
-#     key = list(dicenv.keys())
-#     value = list(dicenv.values())
-#     m = len (dicenv)
-#     for j in range (m):
-#         keyenv = envs[j]
-#         for k in range (m):
-#             if key[k] == keyenv:
-#                 inference.append(value[k])
-#     plt.yscale('log')
-#     # plt.grid(color='k', linestyle='--', linewidth=0.5)
-#     ax = plt.bar(y_pos+(i*bar_width), inference, bar_width)
-#     plt.axis([ -0.25, 5,1E-6, 1])
-#     plt.xticks(range(len(objects)), objects)
-#     # ax.set(ylim=(1E-6, 1E-2))
-
-        
-
-
-# plt.xticks(y_pos, objects)
-# plt.legend(algs,loc=1)
-# plt.ylabel('Inference Time (seconds)')
-# plt.title('Inference Time with 5 CPU 0 GPU')
-
-# plt.show()
-
-# dicenv1 = testenv('1', '1', 'ppo', envs)
-# dicenv2 = testenv('1', '1', 'ars', envs)
-# objects = dicenv1.keys()
-# y_pos = np.arange(len(objects))
-# inference1 = dicenv1.values()
-# inference2 = dicenv2.values()
-# algs = ['PPO', 'ARS']
-# bar_width = 0.2
-# plt.bar(y_pos, inference1, bar_width, alpha=0.5)
-# plt.bar(y_pos+bar_width, inference1, bar_width, alpha=0.5)
-# plt.legend(algs,loc=2)
-# plt.xticks(y_pos, objects)
-# plt.ylabel('Inference Time (seconds)')
-# plt.title('inference time in different environments')
-# plt.show()
-
-
-# 2. same environment same condition, difference of different algorithm
-
-# # envs = ['Hopper-v2','Humanoid-v2','Pusher-v2','continuous','HalfCheetah-v2']
-# envs = ['Hopper-v2','Pusher-v2','continuous','HalfCheetah-v2','Humanoid-v2']
-# # algs = ['ars','sac','ppo','pets','mbpo','planet','rtrl']
-# algs = ['ars', 'sac','ppo','rtrl','mbpo','pets']
-# # algs = ['ars','sac','ppo','mbpo','rtrl']
-# cpus = ['5','4','2','1','0.5','0.1','0.05','0.01']
-# gpu = ['1','0']
-
-# def testenv(cpu, gpu, env, algs, dic = inf_dict):
-#     res = {}
-#     for exp, data in dic.items():
-#         # print(exp,data)
-#         ave_inf = sum(data)/len(data)
-#         exp = exp.split("_")
-#         # print((exp[1] == cpu), (exp[2] == alg and exp[-1] == gpu))
-#         if exp[1] == cpu and exp[2] in algs and exp[-1] == gpu and exp[3] == env:
-#             alg = exp[2]
-#             res[alg] = ave_inf
-#     if env == 'Humanoid-v2':
-#         res['pets'] = 0
-#     return res 
-
-# result = []
-# bar_width = 0.15
-# objects = algs
-# y_pos = np.arange(len(objects))
-# for env in envs:
-#     # for cpu in cpus:
-#     dicenv = testenv('0.05', '0', env, algs)
-#     result.append(dicenv)
-
-# n = len(result)
-# for i in range(n):
-#     inference = []
-#     dicenv = result[i]
-#     print(i)
-#     print(dicenv,type(dicenv))
-#     key = list(dicenv.keys())
-#     value = list(dicenv.values())
-#     m = len (dicenv)
-#     for j in range (m):
-#         keyalg = algs[j]
-#         for k in range (m):
-#             if key[k] == keyalg:
-#                 inference.append(value[k])
-#                 break
-#     # objects = dicenv.keys()
-#     # y_pos = np.arange(len(objects))
-#     plt.yscale('log')
-#     # plt.grid(color='k', linestyle='--', linewidth=0.5)
-#     ax = plt.bar(y_pos+(i*bar_width), inference, bar_width)
-#     plt.axis([ -0.25, 6,1E-6, 10])
-#     plt.xticks(range(len(objects)), objects)
-#     # ax.set(ylim=(1E-6, 1E-2))
-
-        
-
-
-# plt.xticks(y_pos, objects)
-# plt.legend(envs,loc=2)
-# plt.ylabel('Inference Time (seconds)')
-# plt.title('Inference Time with 0.05 CPU 0 GPU')
-
-
-# plt.show()
-
-# 3. same environment same algorithm, difference of different GPU
+wandb.init(project="RTDM_percentage", entity="rt_dm")
+api = wandb.Api()
+env = 'Pusher-v2'
+alg = 'ars'
+seed = '1'
+wconfig = wandb.config
+wconfig.algorithm = alg
+wconfig.eva_seed = seed
+wconfig.env = env
+entity, project = "pierthodo", "RTDM_performance"  # set to your entity and project 
+runs = api.runs(entity + "/" + project) 
+env_step = {'Hopper-v2':0.002,'Pusher-v2':0.01,'continuous_CartPole-v0':0.02,'HalfCheetah-v2':0.01,'Humanoid-v2':0.003}
+algs = ['ars','sac','ppo','rtrl','mbpo','pets']
 envs = ['Hopper-v2','Pusher-v2','continuous','HalfCheetah-v2','Humanoid-v2']
 env_step = {'Hopper-v2':0.002,'Pusher-v2':0.01,'continuous':0.02,'HalfCheetah-v2':0.01,'Humanoid-v2':0.003}
 # envs = ['Hopper-v2','Pusher-v2','continuous','HalfCheetah-v2']
 algs = ['ars','sac','ppo','rtrl','mbpo','pets']
 # algs = ['sac','ppo','mbpo','rtrl']
 # algs = ['ars','sac','ppo','mbpo','rtrl']
-cpus = ['5','1','0.5','0.1','0.05']
+cpus = ['0','0.05','0.1','0.5','1']
 gpu = ['1','0']
 
 def testenv(cpus, gpu, alg, env, dic = inf_dict):
@@ -482,77 +358,100 @@ def testenv(cpus, gpu, alg, env, dic = inf_dict):
     # if alg == 'pets':
     #     res['Humanoid-v2'] = 0
     return res 
-result = []
-bar_width = 0.15
-objects = cpus
-gpu = '0'
-env = 'Hopper-v2'
-y_pos = np.arange(len(objects))
-for alg in algs:
-    # for cpu in cpus:
-    dicenv = testenv(cpus, gpu, alg, env)
-    result.append(dicenv)
-n = len(result)
-for i in range(n):
-    inference = []
-    repeat = []
-    dicenv = result[i]
-    print(i)
-    print(dicenv,type(dicenv))
-    key = list(dicenv.keys())
-    value = list(dicenv.values())
-    m = len (dicenv)
-    for j in range (m):
-        keycpu = cpus[j]
-        for k in range (m):
-            if key[k] == keycpu:
-                # inference.append(value[k])
-                repeat.append((math.floor(value[k]/env_step[env])))
-    plt.yscale('log')
-    # plt.grid(color='k', linestyle='--', linewidth=0.5)
-    # ax = plt.bar(y_pos+(i*bar_width), inference, bar_width)
-    ax = plt.bar(y_pos+(i*bar_width), repeat, bar_width)
-    plt.axis([ -0.25, 5,1, 1E2])
-    plt.xticks(range(len(objects)), objects)
-    # ax.set(ylim=(1E-6, 1E-2))
 
-        
+def cal_repeat(env,gpu = '0'):
+    result = []
+    for alg in algs:
+        # for cpu in cpus:
+        dicenv = testenv(cpus, gpu, alg, env)
+        result.append(dicenv)
+    n = len(result)
+    for i in range(n):
+        repeat = []
+        dicenv = result[i]
+        print(i)
+        print(dicenv,type(dicenv))
+        key = list(dicenv.keys())
+        value = list(dicenv.values())
+        m = len (dicenv)
+        for j in range (m):
+            keycpu = cpus[j]
+            for k in range (m):
+                if key[k] == keycpu:
+                    repeat.append((math.floor(value[k]/env_step[env])))
+    return repeat
 
-plt.xticks(y_pos, objects)
-plt.legend(algs,loc=2)
-plt.hlines(env_step[env],-0.25, 5, 'k', '--')
-plt.ylabel('Inference Time (seconds)')
-plt.xlabel('Number of CPUs')
-plt.title('Inference Time in Pusher-v2')
+repeat = (env)
 
-plt.show()
+summary_list, config_list, name_list = [], [], []
+for run in runs: 
+    # .summary contains the output keys/values for metrics like accuracy.
+    #  We call ._json_dict to omit large files 
+
+    # .config contains the hyperparameters.
+    #  We remove special values that start with _.
+    # for k,v in run.config.items():
+    #     if k =='env' and v == 'Hopper-v2':
+    #         config_list.append({k: v})
+    # print(run.config.items())
+    flag = 0
+    configs = list( run.config.items())
+    wconfig = wandb.config
+    wconfig = run.config.items()
+    if len(configs) and configs[0][1] == env and configs[2][1] == seed  and configs[3][1] == alg:
+        print(configs)
+    # and configs[3][1] == 'sac':
+        config_list.append(
+            {k: v for k,v in run.config.items()
+            if not k.startswith('_')})
+
+    # .name is the human-readable name of the run.
+        name_list.append(run.name)
+        summary_list.append(run.summary._json_dict)
+        logs = run.history()
+        if len(logs): 
+            # print(logs)
+            # repeat = list(logs['Action Repeated'])
+            # percentage = list(logs['Percentage of Reward Decreased'])
+            steps = list(logs['Action Repeated'])
+            percents = list(logs['Percentage of Reward Decreased'])
+            rewards = list(logs['Reward'])
+            # print(rewards,len(rewards))
+            n = len(steps)
+            initial = rewards[0]
+            final= rewards[-1]
+            reward_range = initial-final
+            for i in range (n):
+
+                delay = env_step[env]*i
+                percent1 = percents[i]*100
+                print(i,delay, percents[i])
+                # print(final,rewards[-1])
+                percent = (rewards[i]/initial)*100
+                # print(rewards[i])
+                # if delay > 0.1:
+                #     break
+                wandb.log({"Percentage of Reward Decreased": percent, "Percentage of Reward Decreased1": percent1,"Delay": delay,"Reward":rewards[i],'Environment Step Time':env_step[env]},step=i)
+
+            alg = [configs[3][1]] * n
+            percent_df = logs[['Action Repeated','Percentage of Reward Decreased']]
+            percent_df.insert(2,'Algorithm',alg)
+            flag = 1
+            
+        # print(configs[3])
+        break
+# # print(config_list)
+# runs_df = pd.DataFrame({
+#     "summary": summary_list,
+#     "config": config_list,
+#     "name": name_list
+#     })
+# print(runs_df)
+
+
+# runs_df.to_csv("project.csv")
 
 
 
 
-
-
-
-
-# for exp, data in inf_dict.items():
-#     # print(exp,data)
-#     ave_inf = sum(data)/len(data)
-#     exp = exp.split("_")
-#     if exp[2] == 'ars':
-#         print("====== AES INFERENCE TIME =======")
-#         print(exp[3], ave_inf)
-#     elif exp[2] == 'sac':
-#         print("====== SAC INFERENCE TIME =======")
-#         print(exp[3], ave_inf)
-#     elif exp[2] == 'pets':
-#         print("====== SAC INFERENCE TIME =======")
-#         print(exp[3], ave_inf)
-    # print(x, type(x), x[0])
-    # if exp[1] == 5:
-    #     pass
-    # elif exp[1] == 2:
-    #     pass
-    # elif exp[1] == 1:
-    #     pass
-    # elif exp[1] ==
 
